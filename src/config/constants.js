30e076a51e63d693b280efb1bff557a640abb179
AkosVareso <akosk2626@gmail.com>
2026-06-30 05:47:27 +0200

    Add files via upload

---
+++ src/config/constants.js
@@ -0,0 +1,18 @@
+📁 src/config/constants.js
+require("dotenv").config();
+
+module.exports = {
+    PORT: process.env.PORT || 3000,
+
+    RPC_URLS: [
+        process.env.PRIMARY_RPC_URL,
+        process.env.SECONDARY_RPC_URL,
+        process.env.BACKUP_RPC_URL
+    ],
+
+    USDC_MINT: process.env.USDC_MINT,
+    JUP_MINT: process.env.JUP_MINT,
+
+    WALLET: process.env.WALLET,
+    ATTACK_WALLET: process.env.ATTACK_WALLET
+};
