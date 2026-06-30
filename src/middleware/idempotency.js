6cde58bd2d9e159172ec8265ea499aa17a1ed2fc
AkosVareso <akosk2626@gmail.com>
2026-06-30 05:48:51 +0200

    Add files via upload

---
+++ src/middleware/idempotency.js
@@ -0,0 +1,23 @@
+📁 src/middleware/idempotency.js
+const redis = require("../core/redis");
+
+async function idempotency(key, fn, ttl = 600) {
+    const cached = await redis.get(key);
+
+    if (cached) {
+        return JSON.parse(cached);
+    }
+
+    const result = await fn();
+
+    await redis.set(
+        key,
+        JSON.stringify(result),
+        "EX",
+        ttl
+    );
+
+    return result;
+}
+
+module.exports = idempotency;
