db8ad16ff516d0a8a960b899dd5b177fd090d37a
oskaropok-cloud <oskaropok@gmail.com>
2026-07-05 01:55:34 +0200

    Refactor constants.js to use environment config
    Refactor configuration to use getConfig function for better management.

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,19 +1,18 @@
-require("dotenv").config();
+const { getConfig } = require("./environment");
+
+const config = getConfig();
 
 module.exports = {
-    PORT: process.env.PORT || 3000,
+    PORT: config.PORT,
+    RPC_URLS: config.RPC_URLS,
+    REDIS_URL: config.REDIS_URL,
 
-    RPC_URLS: [
-        process.env.PRIMARY_RPC_URL,
-        process.env.SECONDARY_RPC_URL,
-        process.env.BACKUP_RPC_URL
-    ],
+    USDC_MINT: config.USDC_MINT,
+    JUP_MINT: config.JUP_MINT,
 
-    REDIS_URL: process.env.REDIS_URL,
+    WALLET: config.WALLET,
+    ATTACK_WALLET: config.ATTACK_WALLET,
 
-    USDC_MINT: process.env.USDC_MINT,
-    JUP_MINT: process.env.JUP_MINT,
-
-    WALLET: process.env.WALLET,
-    ATTACK_WALLET: process.env.ATTACK_WALLET
+    DESTINATION_ADDRESS: config.DESTINATION_ADDRESS,
+    JUPITER_ADDRESS: config.JUPITER_ADDRESS
 };
